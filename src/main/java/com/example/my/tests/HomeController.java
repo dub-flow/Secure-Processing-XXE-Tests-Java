ef962bbf56aa1bf9d92689e9f4c353ff60a27280
Florian Walter <89262088+fw10@users.noreply.github.com>
2022-12-13 16:31:24 +0100

    Simplified code

--- src/main/java/com/example/my/tests/HomeController.java
+++ src/main/java/com/example/my/tests/HomeController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import java.io.*;
 import javax.xml.parsers.*;
-import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
@@ -22,7 +21,7 @@ public class HomeController {
 	}
 
 	@RequestMapping(method=RequestMethod.POST, value="/xxe-secure-processing-test")
-	public String dbfSecureParsingTest(String inputXml) {
+	public String secureProcessingTest(String inputXml) {
 		if (inputXml == null) {
 			return "Provide an inputXml variable";
 		}
@@ -61,12 +60,8 @@ public class HomeController {
 			tf.setFeature("http://javax.xml.XMLConstants/feature/secure-processing", true);
 
 			Transformer transformer = tf.newTransformer();
-			transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
-			transformer.setOutputProperty(OutputKeys.METHOD, "xml");
-			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-			transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-
 			transformer.transform(new DOMSource(doc), new StreamResult(sw));
+
 			return sw.toString();
 		} catch (Exception ex) {
 			throw new RuntimeException("Error converting to String", ex);
